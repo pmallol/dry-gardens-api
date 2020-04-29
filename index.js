97d96018212ba64f298df03caf676a86b89427e7
pmallol <pmallol@manas.tech>
2020-04-29 19:50:34 -0300

    Testing CORS settings

--- index.js
+++ index.js
@@ -4,6 +4,24 @@ const port = process.env.PORT || 3000
 
 const gardens = require('./gardens')
 
+const allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
+
+app.configure(function () {  
+  app.use(allowCrossDomain);
+});
+
 app.get('/gardens', (req, res) => res.send(gardens))
 
 app.get('/gardens/:id', (req, res) => {
