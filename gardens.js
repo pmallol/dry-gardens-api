eaca4447d4aa8ca6032d3a56da317fde4257ada6
pmallol <pmallol@manas.tech>
2020-05-12 00:01:15 -0300

    Update gardens

--- gardens.js
+++ gardens.js
@@ -1,52 +1,145 @@
 const gardens = [{
   id: 1,
-  name: 'Daitokuji',
+  name: 'Zuihō-in',
   image:'https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Daitokuji_Kyoto06n3200.jpg/1280px-Daitokuji_Kyoto06n3200.jpg',
   description: '',
-  images: [],
+  images: [{
+    id: 1,
+    path: '/images/zuihouin-1.jpg',
+    source: 'https://sharing-kyoto.com/see_Zuihoin/photos'
+  },
+  {
+    id: 2,
+    path: '/images/zuihouin-2.jpg',
+    source: 'https://sharing-kyoto.com/see_Zuihoin/photos'
+  },
+  {
+    id: 3,
+    path: '/images/zuihouin-3.jpg',
+    source: 'https://sharing-kyoto.com/see_Zuihoin/photos'
+  }],
   web: ''
 }, {
   id: 2,
   name: 'Daisen-in',
   image:'https://upload.wikimedia.org/wikipedia/commons/3/3f/Daisen-in1.jpg',
   description: '',
-  images: [],
+  images: [{
+    id: 1,
+    path: '/images/daisenin-1.jpg',
+    source: ''
+  },
+  {
+    id: 2,
+    path: '/images/daisenin-2.jpg',
+    source: ''
+  },
+  {
+    id: 3,
+    path: '/images/daisenin-3.jpg',
+    source: ''
+  }],
   web: ''
 }, {
   id: 3,
-  name: 'Jishoji',
+  name: 'Jishō-ji',
   image:'https://upload.wikimedia.org/wikipedia/commons/0/06/Ginkakuji_Kyoto03-r.jpg',
   description: '',
-  images: [],
+  images: [{
+    id: 1,
+    path: '/images/jisoji-1.jpg',
+    source: 'https://www.tripadvisor.com/ShowUserReviews-g298564-d321398-r315703073-Ginkakuji_Temple-Kyoto_Kyoto_Prefecture_Kinki.html#photos;aggregationId=101&albumid=101&filter=7&ff=151863286'
+  },
+  {
+    id: 2,
+    path: '/images/jisoji-2.jpg',
+    source: 'https://www.tripadvisor.com/ShowUserReviews-g298564-d321398-r315703073-Ginkakuji_Temple-Kyoto_Kyoto_Prefecture_Kinki.html#photos;aggregationId=101&albumid=101&filter=7&ff=151036074'
+  },
+  {
+    id: 3,
+    path: '/images/jisoji-3.jpg',
+    source: 'https://www.tripadvisor.com/ShowUserReviews-g298564-d321398-r315703073-Ginkakuji_Temple-Kyoto_Kyoto_Prefecture_Kinki.html#photos;aggregationId=101&albumid=101&filter=7&ff=149420696'
+  }],
   web: ''
 }, {
   id: 4,
   name: 'Jisso-in',
   image:'https://upload.wikimedia.org/wikipedia/commons/f/f8/Jissoin-Temple-Stone-Garden.JPG',
   description: '',
-  images: [],
+  images: [{
+    id: 1,
+    path: '/images/',
+    source: ''
+  },
+  {
+    id: 2,
+    path: '/images/',
+    source: ''
+  },
+  {
+    id: 3,
+    path: '/images/',
+    source: ''
+  }],
   web: ''
 }, {
   id: 5,
   name: 'Myoshinji',
   image:'https://upload.wikimedia.org/wikipedia/commons/7/7e/Myoshinji-M9707.jpg',
   description: '',
-  images: [],
+  images: [{
+    id: 1,
+    path: '/images/',
+    source: ''
+  },
+  {
+    id: 2,
+    path: '/images/',
+    source: ''
+  },
+  {
+    id: 3,
+    path: '/images/',
+    source: ''
+  }],
   web: ''
 }, {
   id: 6,
   name: 'Rozanji',
   image:'http://www.taleofgenji.org/images/rozanji_lanterns.jpg',
   description: '',
-  images: [],
+  images: [{
+    id: 1,
+    path: '/images/',
+    source: ''
+  },
+  {
+    id: 2,
+    path: '/images/',
+    source: ''
+  },
+  {
+    id: 3,
+    path: '/images/',
+    source: ''
+  }],
   web: ''
 }, {
   id: 7,
   name: 'Ryoanji',
   image:'https://upload.wikimedia.org/wikipedia/commons/8/80/Ryoanji_Temple_EntranceKyoto.jpg',
   description: '',
-  images: [],
-  web: ''
+  images: [{
+    id: 1,
+    path: '/images/ryoanji-1.jpg',
+    source: 'https://en.wikipedia.org/wiki/File:Kyoto-Ryoan-Ji_MG_4512.jpg'
+  },
+  {
+    id: 2,
+    path: '/images/ryoanji-2.jpg',
+    source: 'https://en.wikipedia.org/wiki/File:RyoanJi-Dry_garden.jpg'
+  }],
+  web: 'http://www.ryoanji.jp/smph/eng/'
 }, {
   id: 8,
   name: 'Tofukuji',
@@ -54,8 +147,18 @@ const gardens = [{
   description: 'The Hôjô (Abbot’s Hall) was rebuilt in 1890 and its surrounding gardens were  designed in 1939 by landscape sculptor Shigemori Mirei (1896-1975). Tôfuku-ji is the only temple in which gardens circumscribe all 4 sides of the Hôjô. A symphony of tradition and abstractionism of modern art and regar-ded as a fine example of contemporary Zen garden, Shigemori’s debut success has been widely introduced to the world. The 4 surrounding gardens that make up the “Hassô” Garden represent “Hassô-jôdô” (the 8 aspects of the Buddha’s life), namely, “Hôrai”, “Hôjô”, “Eijû”, “Koryô”, “Hakkai”, “Gozan”, “Seiden’ichimatsu” and “Hokutoshichisei”. The Southern Garden is composed of a cluster of 4 giant rocks symbolizing the 4 Elysian islands: “Hôrai”, “Hôjô”, “Eijû” and Koryô”, on a bed of swirling raked-gravels that symbolizes “Hakkai”,(the 8 rough sea). To the west, five moss-covered mounts symbolize “Gozan” (the 5 sacred mountains). The Western Garden is a juxtaposition of squarely trimmed azalea shrubs planted against square fields of white gravels to reflect its name “Seiden’ichimatsu”, an ancient Chinese way of land division in the manner of the Chinese character “sei”. The Northern Garden uses foun-dation rocks from the omote-mon (front gate) and moss to manifest an irregu-lar checkered field of green. It ingeniously embodies the colorful autumn foli-age of the Sengyokukan ravine for a scenic background. The Eastern Garden, also named “Hokuto-no-niwa”, distributes the temple’s foundation pillars amidst a bed of gravels to illustrate Hokutoshichisei (the Great Bear constella-tion).',
   images: [{
     id: 1,
-    path: '',
+    path: '/images/tofukuji-1.jpg',
     source: 'https://en.wikipedia.org/wiki/File:Toufuku-ji_hojyo7.JPG'
+  },
+  {
+    id: 2,
+    path: '/images/tofukuji-2.jpg',
+    source: 'https://www.interactiongreen.com/tofukuji-kare-sansui-mirei-shigemori/'
+  },
+  {
+    id: 3,
+    path: '/images/tofukuji-3.jpg',
+    source: 'https://www.interactiongreen.com/tofukuji-kare-sansui-mirei-shigemori/'
   }],
   web: 'http://www.tofukuji.jp/english/'
 }]
